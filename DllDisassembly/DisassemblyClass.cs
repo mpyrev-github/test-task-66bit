ff3aaa414547496183a692c2f6ad6196c3a29282
Mikhail Pyrev <mihail.pyrev@gmail.com>
2019-07-24 01:00:26 +0500

    Fix bugs. Add DLL checking.

--- DllDisassembly/DisassemblyClass.cs
+++ DllDisassembly/DisassemblyClass.cs
@@ -9,44 +9,60 @@ namespace DllDisassembly
         public static void Main(string[] args)
         {
             // Подгружаем все пути сборок с расширением .dll
-            var filePaths = Directory.GetFiles(@"C:\DEV\Assemblies", "*.dll");
-            ShowAssemblyInfo(filePaths);
+            var filePaths = Directory.GetFiles(@"C:\DEV\Assemblies", "*.dll"); 
+            var assemblyInfo = ShowAssemblyInfo(filePaths);
+            // Кодировка для красоты
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(assemblyInfo);
+            Console.ReadLine();
         }
-        private static void ShowAssemblyInfo(string[] filePaths)
+
+        private static string ShowAssemblyInfo(string[] filePaths)
         {
+            string assemblyInfo = "";
             // Пробегаемся по сборкам
             foreach (var filePath in filePaths)
             {
-                try
+                if (CheckDLL(filePath) != true) continue;
+                // Подгружаем сборку
+                var readedAssembly = Assembly.LoadFile(filePath);
+                // Пробегаемся по типам сборки
+                foreach (var type in readedAssembly.GetTypes())
                 {
-                    // Подгружаем сборку
-                    var readedAssembly = Assembly.LoadFile(filePath);
-                    // Пробегаемся по типам сборки
-                    foreach (var type in readedAssembly.GetTypes())
+                    // Если тип является классом, то обрабатываем события
+                    if (type.IsClass)
                     {
-                        // Если тип является классом, то обрабатываем события
-                        if (type.IsClass)
+                        // Вывод названия класса
+                        assemblyInfo += "\u2022 " + type.Name + "\n";
+                        // Получаем методы, которые являются членами экземпляра, публичные и не только,
+                        // унаследованые не учитываем
+                        var methodInfo = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic |
+                                                         BindingFlags.Public | BindingFlags.DeclaredOnly);
+                        // Пробегаемся по методам чтобы их вывести в консоль
+                        foreach (var mi in methodInfo)
                         {
-                            // Кодировка для красоты
-                            Console.OutputEncoding = System.Text.Encoding.UTF8;
-                            // Вывод названия класса
-                            Console.WriteLine("\u2022 " + type.Name);
-                            // Получаем методы, которые являются членами экземпляра, публичные и не только, унаследованые не учитываем
-                            var methodInfo = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
-                            // Пробегаемся по методам чтобы их вывести в консоль
-                            foreach (var mi in methodInfo)
+                            if (mi.IsFamily || mi.IsPublic)
                             {
-                                Console.WriteLine("\t\u25E6 " + mi.Name);
-                            }    
+                                assemblyInfo += "\t\u25E6 " + mi.Name + "\n";
+                            }
                         }
                     }
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
             }
-            Console.ReadLine();
+            return assemblyInfo;
+        }
+
+        private static bool CheckDLL(string filePath)
+        { 
+            try
+            {
+                var assembly = Assembly.LoadFile(filePath);
+                return assembly != null;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
